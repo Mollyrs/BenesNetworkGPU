21c6d8028951d73f63d683d4e6195ead641dba48
Marjan Fariborz <mfariborz@ucdavis.edu>
2020-03-07 22:53:19 -0800

    Routes correctly

--- main.cu
+++ main.cu
@@ -11,7 +11,7 @@
 // includes, project
 #include <cuda.h>
 #include <cuda_runtime.h>
-
+__device__ volatile int fence;
 
 //constucting 8x8 benes network
 //four rows, 5 columns, 20 routers total
@@ -54,25 +54,55 @@ int createMask(int n)
 
 
 __global__
-void benes(int N,  char* network, int* LUT){
-	  int index = blockIdx.x * blockDim.x + threadIdx.x;
+void benes(int N,  char* network, int* LUT, bool* valid, int mask){
 	  int idx = threadIdx.x;
 	  int in1, in2, in1_index, in2_index;
 	  int level = blockIdx.x+1;
-
+        // serializing each block. Puting a fence on the global head for each block
+		while(fence != blockIdx.x);
+			
+	  __syncthreads();
 	  if(blockIdx.x == 0){
-		in1 = network[index*2];
-		in2 = network[index*2+1];
+        if( (valid[idx*2] == 1) && (valid[idx*2+1] == 1)){
+		    in1 = network[idx*2];
+			in2 = network[idx*2+1];
+			if ((in1 & mask) < (in2 & mask)){
+				network[idx*2 + (blockIdx.x+1)*N] = in1;  
+				network[idx*2 + (blockIdx.x+1)*N + 1] = in2;
+			}
+			else{
+				network[idx*2 + (blockIdx.x+1)*N] = in2;  
+				network[idx*2 + (blockIdx.x+1)*N + 1] = in1;
+			}
+			valid[idx*2] = 0;  valid[idx*2 + 1] = 0;
+			valid[idx*2 + (blockIdx.x+1)*N] = 1;   valid[idx*2 + 1 + (blockIdx.x+1)*N] = 1;
+			++fence;
+			__syncthreads();
+		}
 	  }
 	  else {
+		if((valid[idx*2 + (blockIdx.x)*N])==1 && (valid[idx*2 + (blockIdx.x)*N+1]) == 1){
 		  in1_index = LUT[idx*2 + (blockIdx.x-1)*N];
 		  in2_index = LUT[idx*2 + (blockIdx.x-1)*N + 1];
-		  in1 = network[in1_index];
-		  in2 = network[in2_index];
-	  }  
-
-	  network[idx*2 + (blockIdx.x+1)*N] = in1;
-	  network[idx*2 + (blockIdx.x+1)*N + 1] = in2;
+		  in1 = network[in1_index+(blockIdx.x-1)*N];
+		  in2 = network[in2_index+(blockIdx.x-1)*N];
+		  if ((in1 & mask) < (in2 & mask)){
+		  	network[idx*2 + (blockIdx.x)*N] = in1;
+			network[idx*2 + (blockIdx.x)*N + 1] = in2;
+		  }
+		  else{
+			network[idx*2 + (blockIdx.x)*N] = in2;
+			network[idx*2 + (blockIdx.x)*N + 1] = in1;  
+		  }
+		  valid[idx*2 + (blockIdx.x)*N] = 0; valid[idx*2 + 1 + (blockIdx.x)*N] = 0;
+		  valid[idx*2 + (blockIdx.x+1)*N] = 1; valid[idx*2 + 1 + (blockIdx.x+1)*N] = 1;
+		  ++fence;
+		  if (fence ==  2*log2((double)N)-1)
+			  fence = 0;
+            
+		} 
+	  }
+	
 }
 
 
@@ -92,7 +122,6 @@ int main(int argc, char *argv[]){
 
 	
 	int N = atoi(argv[2]);
-	
 	int blockSize = N/2; 
 	int numBlocks = 2*log2((double)N)-1; 
 	int LUTsize = N*(log2((double)N)*2 - 2);
@@ -105,38 +134,43 @@ int main(int argc, char *argv[]){
 	
 	int* LUT;
 	cudaMallocManaged(&LUT,LUTsize*sizeof(int));
-  makeLUT(N,LUT);
+	makeLUT(N,LUT);
+	int mask = createMask(log2((double)N));
   
-  bool *valid;
+    bool *valid;
 	cudaMallocManaged(&valid,N*(numBlocks)*sizeof(bool));
 	memset(valid,0,N*(numBlocks+1)*sizeof(bool)); 
 	for(int i = 0; i < N; i++)
 		valid[i] = 1;
-	
-	benes<<<numBlocks,blockSize>>>(N, network, LUT);
+	benes<<<numBlocks,blockSize>>>(N, network, LUT, valid, mask);
 	cudaDeviceSynchronize();
 	
-	for (int i = 0; i < LUTsize; i++){
-		if (i%N == 0) printf("\n");
-		printf("%d ", LUT[i]);
-	}
-	printf("\n");
 	
-	
-	for (int i = 0; i < N*(numBlocks+1); i++){
+	printf("The input is:");
+	for (int i = 0; i < N; i++){
 		if (i%N == 0) printf("\n");
 		printf("%d ", network[i]);
 	}
   printf("\n");
+  printf("The intermidiate layers are:\n");
+  for (int i = N; i < N*(numBlocks-1); i++){
+	if (i%N == 0) printf("\n");
+	printf("%d ", network[i]);
+}
+printf("\n");
   
-  int mask = createMask(log2((double)N));
 	for (int i = N*(numBlocks-1) ; i < N*(numBlocks); i++){
 		if((mask & network[i]) != i % N){
 			printf("ERROR in routing\n");
 			return 1; 
 		}
 	}
-
+	printf("Routing was successful!\nThe output is:\n");
+	for (int i = N*(numBlocks-1); i < N*(numBlocks); i++){
+		printf("%d ", network[i]);
+	}
+	printf("\n");
+   
 	cudaFree(valid);
 	cudaFree(LUT);
 	cudaFree(network);
